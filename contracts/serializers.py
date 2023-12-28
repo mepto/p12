227eb470434d584d96aee25b6d8f54c1d0998cf9
mepto <natg.inf@gmail.com>
2023-12-28 09:08:03 +0100

    Change client prospect status on contract creation

--- contracts/serializers.py
+++ contracts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from rest_framework.fields import DateTimeField
 
+from clients.models import Client
 from contracts.models import Contract
 
 
@@ -14,3 +15,11 @@ class ContractSerializer(serializers.ModelSerializer):
         """Meta class."""
         model = Contract
         fields = ['id', 'users', 'status_contract', 'client', 'amount', 'due_date', 'date_created', 'date_modified']
+
+    def create(self, validated_data):
+        """Change client prospect status on contract creation."""
+        instance = super().create(validated_data)
+        current_client = Client.objects.get(id=validated_data['client'].id)
+        current_client.is_prospect = False
+        current_client.save()
+        return instance
