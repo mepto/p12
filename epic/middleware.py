f47a5dae759a0f6a39ba6d1b1df0d9f285ea25df
mepto <natg.inf@gmail.com>
2023-12-28 18:01:44 +0100

    Fix issue with logging middleware

--- epic/middleware.py
+++ epic/middleware.py
@@ -1,5 +1,4 @@
 import logging
-from ast import literal_eval
 
 from django.urls import resolve
 from django.utils.deprecation import MiddlewareMixin
@@ -9,11 +8,9 @@ logger = logging.getLogger('main')
 
 class LoggingMiddleware(MiddlewareMixin):
     """Log requests from users."""
-    # def __init__(self, get_response):
-    #     self.get_response = get_response
-    #     # One-time configuration and initialization.
 
     def __call__(self, request):
+        """Call middleware at each reaquest and log non-GET request types."""
         # Code to be executed for each request before
         # the view (and later middleware) are called.
         response = self.get_response(request)
@@ -23,14 +20,12 @@ class LoggingMiddleware(MiddlewareMixin):
                 post.pop('password')
 
             if response['content-type'] == 'application/json':
-                if getattr(response, 'streaming', False):
-                    response_body = '<<<Streaming>>>'
-                else:
-                    response_body = literal_eval(response.content.decode('utf-8'))
+                response_body = response.content.decode('utf-8')
             else:
                 response_body = post
 
-            logger.info('[POST] request by %s (id %s) from %s. Form content: %s', request.user,
-                        request.user.id, resolve(request.path).url_name, response_body)
+            logger.info('[%s] request by %s (id %s) from %s. Form content: %s',
+                        request.method, request.user, request.user.id,
+                        resolve(request.path).url_name, response_body)
 
         return response
